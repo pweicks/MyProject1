73fd69c632acb64a28bc222b937625e6a2122d35
Parker Weicks <pweicks@outlook.com>
2018-04-24 16:48:47 -0600

    My first commit

---
+++ MyProject/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MyProject
+//
+//  Created by Parker Weicks on 4/24/18.
+//  Copyright © 2018 Parker Weicks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        print(text1.text!)
+        print(text2.text!)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
